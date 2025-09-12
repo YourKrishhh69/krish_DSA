1bb4b1ce156eac1dbb6003817c6069905bc92caa
krish69 <krishshah200622@gmail.com>
2025-09-12 15:40:50 +0530

    i create the ex section for dsa practice

--- pointer/part01.cpp
+++ pointer/part01.cpp
@@ -5,7 +5,12 @@ int main(){
     int age = 18;
     int *pointerOFAge = &age;
 
+    //pointer to pointer
+
+    int** pointer2 = &pointerOFAge;
+
     cout<<"address of age is :"<<pointerOFAge<<endl;
+    cout<<"address of pointerOfAge is :"<<pointer2<<endl;
 
     return 0;
 }
